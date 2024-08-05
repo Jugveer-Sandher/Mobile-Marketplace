75fb469d020f7c363e4fb0e386f2b3020e34ef90
Jugveer-Sandher <jugveersandher@gmail.com>
2024-08-05 12:35:50 -0700

    List item component

---
+++ app/components/ListItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+import colors from '../config/colors'
+import AppText from '../components/AppText'
+
+const ListItem = ({ title, subTitle }) => {
+    return (
+        <View style={styles.container}>
+            <AntDesign name="user" size={50} color="black" marginRight={10}/>
+            <View>
+                <AppText style={styles.title}>{title}</AppText>
+                <AppText style={styles.subTitle}>{subTitle}</AppText>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row"
+    },
+    subTitle: {
+        color: colors.medium
+    },  
+    title: {
+        fontWeight: "500"
+    }
+})
+
+export default ListItem;
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -3,6 +3,7 @@ import { View, Image, StyleSheet } from 'react-native';
 
 import AppText from '../components/AppText';
 import colors from '../config/colors'
+import ListItem from '../components/ListItem';
 
 const ListingDetailsScreen = (props) => {
     return (
@@ -11,6 +12,9 @@ const ListingDetailsScreen = (props) => {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Red jacket for sale</AppText>
                 <AppText style={styles.price}>$100</AppText>
+                <View style={styles.userContainer}>
+                    <ListItem title="User" subTitle="7 Listings"/>
+                </View>
             </View>
         </View>
     );
@@ -33,6 +37,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: 500
+    },
+    userContainer: {
+        marginVertical: 40
     }
 })
 
