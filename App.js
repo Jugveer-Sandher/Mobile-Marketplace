0d4f331a011500b46800858b0fba5b9e20f624cf
Jugveer-Sandher <jugveersandher@gmail.com>
2024-08-05 12:39:44 -0700

    updated view image screen icons

--- App.js
+++ App.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { View } from 'react-native'
 
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />
+  return <ViewImageScreen />
 }
