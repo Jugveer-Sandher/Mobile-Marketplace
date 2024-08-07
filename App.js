2f4daf77f6c37c6cf89b7611823a308bcd4e1411
Jugveer-Sandher <jugveersandher@gmail.com>
2024-08-07 14:18:55 -0700

    screen component & message screen

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { View } from 'react-native'
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessageScreen from './app/screens/MessageScreen';
 
 export default function App() {
-  return <ViewImageScreen />
+  return <MessageScreen  />
 }
---
+++ app/components/Screen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Platform, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
+
+const Screen = ({ children }) => {
+    return (
+        <SafeAreaView style={styles.screen}>
+            {children}
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0
+    }
+})
+
+export default Screen;
